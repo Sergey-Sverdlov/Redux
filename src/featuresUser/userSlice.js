6a6e06d9f8571c914da9073b674f5d28ecd8e558
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-01-17 02:21:50 +0300

    add slicer, dispatch and selector for name and lastName

--- src/featuresUser/userSlice.js
+++ src/featuresUser/userSlice.js
@@ -1,1 +1,25 @@
 import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    firstName: "",
+    lastName: ""
+}
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        setFirstName: (state, action) => {
+            state.firstName = action.payload
+        },
+        setLastName: (state, action) => {
+            state.lastName = action.payload
+        }
+    },
+})
+
+export const {setFirstName} = userSlice.actions
+
+export const {setLastName} = userSlice.actions
+
+export default userSlice.reducer
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit";
+import userSlice from "../featuresUser/userSlice";
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        user: userSlice,
+    }
 })
