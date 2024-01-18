15bb587d8fbd245a64eced17eb9a1e5b1348253a
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-01-19 02:08:21 +0300

    add todolist with completeToggle and delete todo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,30 @@
 import React from 'react'
+import {useDispatch} from "react-redux";
+import {toggleCompletedTodo} from "../features/todo/todoSlice";
+import {removeTodo} from "../features/todo/todoSlice";
 
-const TodoItem = () => {
-    const completed = false
+const TodoItem = ({todo}) => {
+    const dispatch = useDispatch()
+    const toggleTodoHandler = (id) => {
+        dispatch(toggleCompletedTodo(id))
+    }
+    const removeHandler = (id) => {
+        dispatch(removeTodo(id))
+    }
     return (
         <div className='flex justify-between items-center my-2'>
-            <div className='text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400'>
+            <div
+                className='text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400'
+                onClick={() => toggleTodoHandler(todo.id)}
+            >
                 Complete
             </div>
-            <div className={`text-sm ${completed ? 'line-through font-medium text-lime-400' : ''}`}>
-                Todo Text
+            <div className={`text-sm ${todo.completed ? 'line-through font-medium text-lime-400' : ''}`}>
+                {todo.text}
             </div>
-            <div className='text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer'>
+            <div
+                onClick={() => removeHandler(todo.id)}
+                className='text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer'>
                 Delete
             </div>
         </div>
